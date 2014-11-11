6d6b2c2e517e16f0a780e95cac0e7069b59df71a
Leoxur <Leoxur@users.noreply.github.com>
2014-11-11 21:30:47 +0100

    Update map_insert.c

--- map_insert.c
+++ map_insert.c
@@ -6,11 +6,13 @@ void clear_screen ();
 void draw_map ();
 void map_null ();
 void insert_player ();
+void insert_pc ();
 
 char mappa[3][3];   //Real map
 int appo_map[3][3]; //Buffermap
 int turn = 0;   //0 = Player turn, 1 = Pc turn
 int c = 0;
+int pc = 0;
 
 
 int main (){
@@ -19,21 +21,23 @@ int main (){
     map_null();
     do{
         switch(turn){
-           // case 0: {
+            case 0: {
                     clear_screen();
                     draw_map();
                     insert_player();
-                    turn = 0;
+                    turn = 1;
                     counter++;
-                   // }; break;
+                    }; break;
 
-              /*case 1: {//insert_pc();
+            case 1: {
+                    clear_screen();
                     draw_map();
+                    insert_pc();
                     turn = 0;
                     counter++;
                     }; break;
 
-        }*/
+        }
     }while(counter <= 9);
 return 0;
 }
@@ -70,6 +74,40 @@ void insert_player(){
             else {
             printf("\nReinserire un valore\n");
             insert_player();
+            }
+        turn = 1;
+}
+
+void insert_pc(){
+
+    srand(9);
+    pc = rand() %9;
+    if ( pc > 0 && c < 10 )
+                switch (pc){
+                    case  1:{ appo_map[2][0]=pc;
+                                 mappa[2][0]= 'O';}   break;
+                    case  2:{ appo_map[2][1]=pc;
+                                 mappa[2][1]= 'O';}   break;
+                    case  3:{ appo_map[2][2]=pc;
+                                 mappa[2][2]= 'O';}   break;
+
+                    case  4:{ appo_map[1][0]=pc;
+                                 mappa[1][0]= 'O';}   break;
+                    case  5:{ appo_map[1][1]=pc;
+                                 mappa[1][1]= 'O';}   break;
+                    case  6:{ appo_map[1][2]=pc;
+                                 mappa[1][2]= 'O';}   break;
+
+                    case  7:{ appo_map[0][0]=pc;
+                                 mappa[0][0]= 'O';}   break;
+                    case  8:{ appo_map[0][1]=pc;
+                                 mappa[0][1]= 'O';}   break;
+                    case  9:{ appo_map[0][2]=pc;
+                                 mappa[0][2]= 'O';}   break;
+                }
+            else {
+            printf("\nReinserire un valore\n");
+            insert_pc();
             }
         turn = 1;
 }
