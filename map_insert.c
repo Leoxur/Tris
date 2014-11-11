d07ba7c1db4874263bfc20cabacc83aad6282ac4
Leoxur <Leoxur@users.noreply.github.com>
2014-11-11 21:19:53 +0100

    Update map_insert.c

--- map_insert.c
+++ map_insert.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void clear_screen ();
 void draw_map ();
 void map_null ();
 void insert_player ();
-void clear_screen ();
-char mappa[2][2];   //Real map
-int appo_map[2][2]; //Buffermap
-int turn = 0;   //0 = Player turn, 1 = Pc turn
 
+char mappa[3][3];   //Real map
+int appo_map[3][3]; //Buffermap
+int turn = 0;   //0 = Player turn, 1 = Pc turn
+int c = 0;
 
 
 int main (){
 
     int counter = 0; //contatore mosse, massimo a 9
     map_null();
-    draw_map();
     do{
-        //switch(turn){
+        switch(turn){
            // case 0: {
                     clear_screen();
                     draw_map();
@@ -27,84 +27,82 @@ int main (){
                     counter++;
                    // }; break;
 
-           /* case 1: {//insert_pc();
+              /*case 1: {//insert_pc();
                     draw_map();
                     turn = 0;
                     counter++;
-                    }; break;*/
+                    }; break;
 
-       // }
-    }while(counter < 9);
+        }*/
+    }while(counter <= 9);
+return 0;
 }
 
 void insert_player(){
 
-    int c = 0;
-    printf("Inserire un numero da 1 a 9 col tastierino \n(gioca con il tastierino come fosse la mappa di gioco)\n");
+    printf("\nInserire un numero da 1 a 9 col tastierino\n");
+    printf("[gioca con il tastierino come fosse la mappa di gioco]\n");
     scanf ("%d", &c);
-    int i = 0, j = 0;
-    for (i=0; i<3; i++ )
-        for (j=0; j<3; j++)
-            do{
-            switch (c){
-                case  1:{ appo_map[2][0]=1;
-                             mappa[2][0]= 'X';}   break;
-                case  2:{ appo_map[2][1]=1;
-                             mappa[2][1]= 'X';}   break;
-                case  3:{ appo_map[2][2]=1;
-                             mappa[2][2]= 'X';}   break;
 
-                case  4:{ appo_map[1][0]=1;
-                             mappa[1][0]= 'X';}   break;
-                case  5:{ appo_map[1][1]=1;
-                             mappa[1][1]= 'X';}   break;
-                case  6:{ appo_map[1][2]=1;
-                             mappa[1][2]= 'X';}   break;
+            if ( c > 0 && c < 10 )
+                switch (c){
+                    case  1:{ appo_map[2][0]=c;
+                                 mappa[2][0]= 'X';}   break;
+                    case  2:{ appo_map[2][1]=c;
+                                 mappa[2][1]= 'X';}   break;
+                    case  3:{ appo_map[2][2]=c;
+                                 mappa[2][2]= 'X';}   break;
 
-                case  7:{ appo_map[0][0]=1;
-                             mappa[0][0]= 'X';}   break;
-                case  8:{ appo_map[0][1]=1;
-                             mappa[0][1]= 'X';}   break;
-                case  9:{ appo_map[0][2]=1;
-                             mappa[0][2]= 'X';}   break;
+                    case  4:{ appo_map[1][0]=c;
+                                 mappa[1][0]= 'X';}   break;
+                    case  5:{ appo_map[1][1]=c;
+                                 mappa[1][1]= 'X';}   break;
+                    case  6:{ appo_map[1][2]=c;
+                                 mappa[1][2]= 'X';}   break;
+
+                    case  7:{ appo_map[0][0]=c;
+                                 mappa[0][0]= 'X';}   break;
+                    case  8:{ appo_map[0][1]=c;
+                                 mappa[0][1]= 'X';}   break;
+                    case  9:{ appo_map[0][2]=c;
+                                 mappa[0][2]= 'X';}   break;
+                }
+            else {
+            printf("\nReinserire un valore\n");
+            insert_player();
             }
-            c= 0;
-            }while ( c<0 || c>9);
-            turn = 1;
+        turn = 1;
 }
 
 void draw_map(){
-printf("In function draw_map");
-     int i = 0, j = 0;
+
+    printf("\n\t     ..-Drawing Maps-..\n");
+    int i = 0, j = 0;
     for (i=0; i<3; i++ ){
-        printf("\n\t\t-------------\n");
-        printf("\t\t| ");
+        printf("\n\t\t-------------\n\t\t| ");
         for (j=0; j<3; j++){
-            /*if (appo_map[i][j] == 1)
+        /*    if (appo_map[i][j] == 1)
 			    printf ("| O ");
 			else if (appo_map[i][j] == 2)
                 printf("| X ");
             else
 				printf (" %c | ", mappa [i][j]);*/
             printf(" %c ", mappa[i][j]);
-            }printf(" |");
+            }
+        printf(" |");
     }
-        printf("\n\t\t-------------\n");
+    printf("\n\t\t-------------\n");
 }
-
-
-
+//Initialising map
 void map_null(){
 
      int i = 0, j = 0;
      for (i=0; i<3; i++)
         for (j=0; j<3; j++){
             mappa[i][j]= ' ';
-            //printf(" %c\n ", mappa[i][j]);
         }
 }
-
-
+//Clear screen
 void clear_screen(){
 
     const char* CLEAR_SCREE_ANSI = "\e[1;1H\e[2J";
